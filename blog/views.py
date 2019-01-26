b0bc7b471f28d1d76e35e5717bd71114d65f3f4a
Anne A <aaguirre.rdit@gmail.com>
2019-01-26 20:27:45 +0900

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from .models import Post, Category
 from django.utils import timezone
+from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
 from .forms import ContactForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    postlist = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     categories = Category.objects.filter()
+    paginator = Paginator(postlist, 2)  # Show 10 contacts per page
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     return render(request, 'blog/post_list.html', {'posts':posts, 'categories':categories})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
